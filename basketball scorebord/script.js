1f105e5ee16d1b5112a421369c78806be65b0436
Asenakew Zegeye <asenakew.zegeye@medieinstitute.se>
2022-12-02 22:41:23 +0100

    new basketball score counting bord

---
+++ basketball scorebord/script.js
@@ -0,0 +1,49 @@
+let result = 0;
+document.getElementById("result-el").textContent = result;
+
+let resultEl = document.getElementById("result-el");
+
+
+function add1(){
+  
+    result = result + 1;
+     resultEl.textContent = result;
+    
+}
+
+function add2(){
+  
+     result = result + 2;
+     resultEl.textContent = result;
+    
+}
+function add3(){
+  
+     result = result + 3;
+    resultEl.textContent = result;
+    
+}
+document.getElementById("gustResult").textContent = result;
+
+let resultEl1 = document.getElementById("gustResult");
+
+
+function add11(){
+  
+    result = result + 1;
+    gustResult.textContent = result;
+    
+}
+
+function add21(){
+  
+     result = result + 2;
+     gustResult.textContent = result;
+    
+}
+function add31(){
+  
+     result = result + 3;
+     gustResult.textContent = result;
+    
+}
